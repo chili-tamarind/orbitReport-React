d0ea3d8b2c5a326a5f55d9de5233fd8c4bab51c9
chilli-tamarind <ceasinugo@randolphcollege.edu>
2024-08-11 22:03:54 -0500

    Class 16 Updates

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,30 @@
+import styles from './styling.css';
+import satData from './satData';
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-    </div>
+  
+  const selectOrbit = (
+    <button className="selectSatOrbit">{displaySats.map((sat, id) => {
+      return (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+        );
+      })}
+    </button>
   );
+  
+  return (
+    <div className="flex-container">
+      {selectOrbit}
+
+      <button className="allOrbit" onClick={() => setSat(satData)}>
+        All Orbit
+      </button>
+      
+    </div>
+  );     
+    
 };
 
 export default Buttons;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,63 @@
 const Table = ({ sat }) => {
+
+  const orbitRows = sat.map((data, id) => (
+    <tr key={data.id}>
+      <td>{data.name}</td>
+      <td>{data.type}</td>
+      <td>{data.launchDate}</td>
+      <td>{data.operational ? "Active" : "Inactive"}</td>
+    </tr>
+  ));  
+
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th> Name</th>
+          <th> Type of Satellite</th>
+          <th> Launch Date</th>
+          <th> Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {orbitRows}
+      </tbody>
+    </table>
   );
 };
 
 export default Table;
+
+// const Table = ({ sat }) => {
+
+//   const orbitRows = (
+//     <tr>
+//       {sat.map((data, id) => (
+//         <td key={id}>{data}</td>
+//       ))}
+//     </tr>
+//   );
+  
+
+
+//   return (
+//     <table>
+//       <thead>
+//         <tr>
+//           <th> Name</th>
+//           <th> Type of Satellite</th>
+//           <th> Launch Date</th>
+//           <th> Status</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         <tr>
+//           <td>Row Data TBD</td>
+//           <td>Row Data TBD</td>
+//           <td>Row Data TBD</td>
+//           <td>Row Data TBD</td>
+//         </tr>
+//       </tbody>
+//     </table>
+//   );
+// };
