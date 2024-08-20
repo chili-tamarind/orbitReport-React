57e1c69fa20ab1dabea8adf9d9061a44b7d44835
chilli-tamarind <ceasinugo@randolphcollege.edu>
2024-08-19 21:38:09 -0500

    Class 18 Updates

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,21 +4,20 @@ import satData from './satData';
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   
   const selectOrbit = (
-    <button className="selectSatOrbit">{displaySats.map((sat, id) => {
+    <div >{displaySats.map((sat, id) => {
       return (
         <button onClick={() => filterByType(sat)} key={id}>
           {sat} Orbit
         </button>
         );
       })}
-    </button>
+    </div>
   );
   
   return (
     <div className="flex-container">
       {selectOrbit}
-
-      <button className="allOrbit" onClick={() => setSat(satData)}>
+      <button onClick={() => setSat(satData)}>
         All Orbit
       </button>
       
